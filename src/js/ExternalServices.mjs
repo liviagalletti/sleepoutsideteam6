46e62cf214e213059769a36fb450cddd191c987a
liviagalletti <liligalletti@gmail.com>
2024-07-11 15:45:04 -0300

    ret

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,5 +1,4 @@
-const baseURL = window.location.protocol === 'https:' ? 'https://server-nodejs.cit.byui.edu:3000/' : 'http://server-nodejs.cit.byui.edu:3000/';
-
+const baseURL = "http://server-nodejs.cit.byui.edu:3000/";
 async function convertToJson(res) {
   const data = await res.json();
   if (res.ok) {
